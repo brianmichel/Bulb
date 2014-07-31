d4c0d998b4c0e6cf3c42bd83688daf36b05162b0
Brian Michel <brian@tumblr.com>
2014-07-30 20:09:14 -0400

    Initial Commit

---
+++ Bulb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bulb
+//
+//  Created by Brian Michel on 7/30/14.
+//  Copyright (c) 2014 Brian Michel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
